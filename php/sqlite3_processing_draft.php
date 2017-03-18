cb88c5b4c48353d686035f0ce5dc15a4ae6feeff
Delvin So <0ds54@queensu.ca>
2017-03-18 14:29:19 -0400

    Update sqlite3_processing_draft.php

--- php/sqlite3_processing_draft.php
+++ php/sqlite3_processing_draft.php
@@ -1,4 +1,5 @@
-//draft php script for processing user input fields into an sqlite3 database 
+//draft php script for processing user input fields into an sqlite3 database
+//todo : validation and statement prepartion
 
 <?php
 <html>
@@ -85,59 +86,145 @@ for ($i = 0; $i < count($tableNames); $i++){
   }
 }
 
+//want to create a an array of array so when the user submits the information,
+//we can create insert statements without having to create a for loop for each table
+// $AoA = array(
+// array($relationToReporter, $incidenceDescription,$locationType), //harassment incidence
+// array($typeSexual,$typeWorkplace,$typeStreet, $typeSchool,$typeOnline,$typeReligious), //harassment type
+// array($perpGender, $perpAge,$perpSexuality, $perpEthnicity), //perp info
+// array($caseGender,$caseAge,$caseSexuality,$caseEthnicity) //case info
+// );
 
-  $harassmentIncidence=$array($relationToReporter, $incidenceDescription,$locationType);
+//need some form validation to prevent SQL injection...
 
-  $relationToReporter = $_POST['relation'];
-  $incidenceDescription= $_POST['incidenceDescription'];
-  $locationType= $_POST['location'];
-
-  //escaping to prevent SQL inejction
-  $relationToReporter = mysql_real_escape_string($relationToReporter);
-  $incidenceDescription = mysql_real_escape_string($incidenceDescription);
-  $locationType = mysql_real_escape_string($locationType);
-
-
-  $harassmentType=$array($typeSexual,$typeWorkplace,$typeStreet, $typeSchool,$typeOnline,$typeReligious);
-
-  $typeSexual = $_POST[];
-  $typeWorkplace = $_POST[];
-  $typeStreet = $_POST[];
-  $typeSchool = $_POST[];
-  $typeOnline = $_POST[];
-  $typeReligious = $_POST[];
-
-
-  $perpInfo=$array($perpGender, $perpAge,$perpSexuality, $perpEthnicity);
-
-  $perpGender = $_POST[];
-  $perpAge = $_POST[];
-  $perpSexuality = $_POST[];
-  $perpEthnicity = $_POST[];
-
-  //variables for PERP_INFO
-
-  $caseInfo=$array($caseGender,$caseAge,$caseSexuality,$caseEthnicity);
-
-  $caseGender = $_POST[];
-  $caseAge = $_POST[];
-  $caseSexuality = $_POST[];
-  $caseEthnicity = $_POST[];
-  //variables for caseInfo
-
-
-
-
-
-  $insertStatement=<<<EOF
-    INSERT INTO $harassment_incidence
-    (incidenceDescription, locationType, relationToReporter)
-    VALUES(
-    'incidence_description',
-    'downtown',
-    'stranger'
-    );
+//inserting user input fields into harassment incidence
+$prep = $db->prepare($insertStatement);
+$insertStatement=<<<EOF
+  INSERT INTO $harassmentIncidence(incidenceDescription, locationType, relationToReporter)
+  VALUES(?,?,?);
 EOF;
+$insertStatement->bind_param("sss",$relationToReporter, $incidenceDescription, $locationType);
+
+
+$relationToReporter = $_POST['relation'];
+$incidenceDescription= $_POST['incidenceDescription'];
+$locationType= $_POST['location'];
+
+$prep->execute(); //not sure of syntax..
+
+//inserting user input fields into harassment type
+$prep = $db->prepare($insertStatement);
+$insertStatement=<<<EOF
+  INSERT INTO $harassmentType(sexual, workplace, street,school,online,religious)
+  VALUES(?,?,?,?,?,?);
+EOF;
+$insertStatement->bind_param("iiiiii",$typeSexual,$typeWorkplace,$typeStreet, $typeSchool,$typeOnline,$typeReligious) //swap
+
+$typeSexual = $_POST[];
+$typeWorkplace = $_POST[];
+$typeStreet = $_POST[];
+$typeSchool = $_POST[];
+$typeOnline = $_POST[];
+$typeReligious = $_POST[];
+
+
+$prep->execute();
+
+
+//inserting user input fields into perp info
+$prep = $db->prepare($insertStatement);
+$insertStatement=<<<EOF
+  INSERT INTO $perpInfo(gender,age,sexuality,ethnicity)
+  VALUES(?,?,?,?;)
+EOF;
+$insertStatement->bind_param("ssss",$perpGender, $perpAge,$perpSexuality, $perpEthnicity);
+
+$perpGender = $_POST[];
+$perpAge = $_POST[];
+$perpSexuality = $_POST[];
+$perpEthnicity = $_POST[];
+
+$prep->execute();
+
+
+//inserting user input fields into case info
+$prep = $db->prepare($insertStatement);
+$insertStatement=<<<EOF
+  INSERT INTO $caseInfo(gender,age,sexuality,ethnicity)
+  VALUES(?,?,?,?;)
+EOF;
+$insertStatement->bind_param("ssss",$caseGender, $caseAge,$caseSexuality, $caseEthnicity);
+
+$caseGender = $_POST[];
+$caseAge = $_POST[];
+$caseSexuality = $_POST[];
+$caseEthnicity = $_POST[];
+
+$prep->execute();
+
+
+  //
+  // $harassmentIncidence=$array($relationToReporter, $incidenceDescription,$locationType);
+  //
+  // $relationToReporter = $_POST['relation'];
+  // $incidenceDescription= $_POST['incidenceDescription'];
+  // $locationType= $_POST['location'];
+  //
+  // //escaping to prevent SQL injection
+  // $relationToReporter = mysql_real_escape_string($relationToReporter);
+  // $incidenceDescription = mysql_real_escape_string($incidenceDescription);
+  // $locationType = mysql_real_escape_string($locationType);
+  //
+  //
+  // $harassmentType=$array($typeSexual,$typeWorkplace,$typeStreet, $typeSchool,$typeOnline,$typeReligious);
+  //
+  // $typeSexual = $_POST[];
+  // $typeWorkplace = $_POST[];
+  // $typeStreet = $_POST[];
+  // $typeSchool = $_POST[];
+  // $typeOnline = $_POST[];
+  // $typeReligious = $_POST[];
+  //
+  // //escaping to prevent SQL injection
+  // $typeSexual = mysql_real_escape_string($typeSexual);
+  // $typeWorkplace=mysql_real_escape_string($typeWorkplace);
+  // $typeStreet=mysql_real_escape_string($typeStreet);
+  // $typeSchool=mysql_real_escape_string($typeSchool);
+  // $typeOnline=mysql_real_escape_string($typeOnline);
+  // $typeReligious=mysql_real_escape_string($typeReligious);
+  //
+  //
+  // $perpInfo=$array($perpGender, $perpAge,$perpSexuality, $perpEthnicity);
+  //
+  // $perpGender = $_POST[];
+  // $perpAge = $_POST[];
+  // $perpSexuality = $_POST[];
+  // $perpEthnicity = $_POST[];
+  //
+  // //variables for PERP_INFO
+  //
+  // $caseInfo=$array($caseGender,$caseAge,$caseSexuality,$caseEthnicity);
+  //
+  // $caseGender = $_POST[];
+  // $caseAge = $_POST[];
+  // $caseSexuality = $_POST[];
+  // $caseEthnicity = $_POST[];
+  //
+
+
+
+//   $insertStatement=<<<EOF
+//     INSERT INTO $harassmentIncidence(incidenceDescription, locationType, relationToReporter)
+//     VALUES(
+//     'incidence_description',
+//     'downtown',
+//     'stranger'
+//     );
+// EOF;
+
+
+
+
 
   $insert=$db->exec($insertStatement);
   if (!$insert){
